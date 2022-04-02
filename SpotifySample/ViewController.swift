6e3c5ce5bcfd4cacb673fdab1ef19917b5617294
anies1212 <all.for.me1212sna@gmail.com>
2022-04-02 21:23:01 +0900

    Initial Commit

---
+++ SpotifySample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SpotifySample
+//
+//  Created by anies1212 on 2022/04/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
